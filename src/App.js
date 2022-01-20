cc92beb077e78ed02708059c7439fc7898fdd2a7
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-20 09:17:30 -0800

    first attempt at rendering ribbon with our zfin data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import useFetch from 'use-http';
+import React from "react";
+
 function App() {
+  const dataSource = "https://cell-mac.zfin.org:8080/action/api/marker/ZDB-GENE-010320-2/go/ribbon-summary";
+  const options = {} // these options accept all native `fetch` options
+  // the last argument below [] means it will fire onMount (GET by default)
+  const { loading, error, ribbon = [] } = useFetch(dataSource, options, []);
+  const compareOrthologs = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Testing Ribbon
+      </h2>
+      <wc-ribbon-strips
+          category-all-style='1'
+          color-by='0'
+          data={JSON.stringify(ribbon)}
+          fire-event-on-empty-cells={false}
+          group-clickable={false}
+          group-open-new-tab={false}
+          id='go-ribbon'
+          new-tab={false}
+          selection-mode='0'
+          show-other-group
+          subject-base-url='/gene/'
+          subject-open-new-tab={false}
+          subject-position={compareOrthologs ? '1' : '0'}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyPolyfills, defineCustomElements } from '@geneontology/wc-ribbon-strips/loader';
+import { applyPolyfills as applyPolyfills2, defineCustomElements as defineCustomElements2 } from '@geneontology/wc-ribbon-table/loader';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+applyPolyfills().then(() => {
+    defineCustomElements(window);
+});
+
+applyPolyfills2().then(() => {
+    defineCustomElements2(window);
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
