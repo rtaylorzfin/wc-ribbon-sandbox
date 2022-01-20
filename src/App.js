50bc630110f04de152073cf8c6f692f531a8bb7f
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-20 11:45:53 -0800

    fetch initial data from local dev environment

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
-
 import useFetch from 'use-http';
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [ribbon, setRibbon] = useState(null);
+  const [isReady, setIsReady] = useState(false);
   const dataSource = "https://cell-mac.zfin.org:8080/action/api/marker/ZDB-GENE-010320-2/go/ribbon-summary";
-  const options = {} // these options accept all native `fetch` options
-  // the last argument below [] means it will fire onMount (GET by default)
-  const { loading, error, ribbon = [] } = useFetch(dataSource, options, []);
-  const compareOrthologs = true;
+  const { get, post, response, loading, error } = useFetch(dataSource);
 
+  useEffect(() => { loadInitialRibbon() }, []); // componentDidMount
+
+  function loadInitialRibbon() {
+    get()
+        .then(data => {
+            console.log('ribbon load');
+            console.log(data);
+            console.log(response);
+
+            if (response.ok) {
+              // setRibbon({ribbonSummary: data});
+              setRibbon(data);
+              setIsReady(true);
+            }
+          }
+        );
+  }
+
+  const compareOrthologs = true;
+  console.log("ribbon object", ribbon);
   return (
     <div className="App">
       <h2>
         Testing Ribbon
       </h2>
-      <wc-ribbon-strips
-          category-all-style='1'
-          color-by='0'
-          data={JSON.stringify(ribbon)}
-          fire-event-on-empty-cells={false}
-          group-clickable={false}
-          group-open-new-tab={false}
-          id='go-ribbon'
-          new-tab={false}
-          selection-mode='0'
-          show-other-group
-          subject-base-url='/gene/'
-          subject-open-new-tab={false}
-          subject-position={compareOrthologs ? '1' : '0'}
-      />
+        {!isReady && <div>Loading...</div>}
+        {isReady &&
+          <wc-ribbon-strips
+              category-all-style='1'
+              color-by='0'
+              data={JSON.stringify(ribbon)}
+              fire-event-on-empty-cells={false}
+              group-clickable={false}
+              group-open-new-tab={false}
+              id='go-ribbon'
+              new-tab={false}
+              selection-mode='0'
+              show-other-group
+              subject-base-url='/gene/'
+              subject-open-new-tab={false}
+              subject-position={compareOrthologs ? '1' : '0'}
+          />}
     </div>
   );
 }
